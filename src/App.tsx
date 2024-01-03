8da3111457823afc3d1c4716446203d0343b262e
sidhant <sidhantwadhwani@gmail.com>
2024-01-03 15:34:47 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-function App() {
-  return <Button colorScheme="blue">Button</Button>;
-}
+const App = () => {
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
+};
 
 export default App;
+
+// Version - 5 - Installing Chakra UI.mp4
+
+// import { Button, ButtonGroup } from "@chakra-ui/react";
+
+// function App() {
+//   return <Button colorScheme="blue">Button</Button>;
+// }
+
+// export default App;
