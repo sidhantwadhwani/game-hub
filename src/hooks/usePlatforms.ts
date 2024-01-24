823c6b89c0792fc00618d584162c27bd6de570bb
sidhant <sidhantwadhwani@gmail.com>
2024-01-24 14:33:59 +0100

    Ship genres and platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,8 @@
 import useData from "./useData";
 import { Platform } from "./useGames";
+import platforms from "../data/platforms";
 
-// export interface Platforms {
-//     id : number;
-//     name : string;
-//     slug : string;
 
-// }
-
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => ({data : platforms, error : null})
 
 export default usePlatforms;
